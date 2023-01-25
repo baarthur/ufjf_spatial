342e7975854de472abb2e1eb113360593ea10846
baarthur <baarthur0@outlook.com>
2023-01-25 13:23:06 -0300

    projeto

---
+++ src/0_render_output.R
@@ -0,0 +1,4 @@
+rmarkdown::render(
+  input = here::here("src/2_seminario_nakamura2021.Rmd"),
+  output_format = "beamer_presentation",
+  output_file = here::here("output/Seminário - Nakamura 2021"))
